bde9b11fe72cfe40096246f41a88def99d047d71
Dardan Bujupaj <dardan@ax.tech>
2022-11-10 17:50:42 +0100

    add mouseholes (#13)
    * add mouse graphics
    * find a target mousehole when a mouse spawns
    * track number of mouses per mousehole

--- Mouse.gd
+++ Mouse.gd
@@ -5,17 +5,35 @@ var attraction = -20
 const MAX_SPEED = 500
 
 
+var target: Node2D
+
 var velocity: Vector2
 var direction := Vector2.RIGHT
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var mouseholes = get_tree().get_nodes_in_group("mouseholes")
+	mouseholes.shuffle()
+	
+	for mousehole in mouseholes:
+		if to_local(mousehole.global_position).length() > 1:
+			target = mousehole
+			return
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta: float) -> void:
+	$Mouse.flip_h = velocity.x < 0
+	if to_local(target.global_position).length() < 10:
+		target.mouses += 1
+		queue_free()
 
 
 func _physics_process(delta: float) -> void:
-	velocity = velocity.linear_interpolate(direction * MAX_SPEED, delta * 20)
+	if target != null:
+		velocity = to_local(target.global_position).normalized() * MAX_SPEED
+	else:
+		velocity = velocity.linear_interpolate(direction * MAX_SPEED, delta * 20)
+	
 	position += velocity * delta
 
 
