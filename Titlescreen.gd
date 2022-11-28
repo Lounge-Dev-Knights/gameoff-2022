b1bc23e211022a8b7933f0c3012c3c67c02ded6f
Dardan Bujupaj <dardan@ax.tech>
2022-11-28 10:11:36 +0100

    Scene transition (#49)
    * add scene loader
    
    * add titlescreen
    
    * fix: add scene transition

--- Titlescreen.gd
+++ Titlescreen.gd
@@ -2,19 +2,19 @@ extends CenterContainer
 
 
 var levels = [
-	preload("res://levels/Testlevel.tscn"),
-	preload("res://levels/Level_1.tscn"),
-	preload("res://levels/Level_2.tscn"),
-	preload("res://levels/Level_3.tscn"),
-	preload("res://levels/Level_4.tscn"),
+	"res://levels/Testlevel.tscn",
+	"res://levels/Level_1.tscn",
+	"res://levels/Level_2.tscn",
+	"res://levels/Level_3.tscn",
+	"res://levels/Level_4.tscn",
 ]
 
 
 func _ready():
 	for level in levels:
 		var button = Button.new()
-		button.text = (level.resource_path as String).get_file().get_basename()
-		button.connect("pressed", get_tree(), "change_scene_to", [level])
+		button.text = level.get_file().get_basename()
+		button.connect("pressed", SceneLoader, "goto_scene", [level])
 		$VBoxContainer.add_child(button)
 
 
