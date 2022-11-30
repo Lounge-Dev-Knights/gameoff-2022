0aa2f209a3b34d0dcdee67eb1c41dc6442c0bfdb
Dardan Bujupaj <dardan@ax.tech>
2022-11-30 21:11:36 +0100

    add sound when items are purchased

--- ItemSelectionItem.gd
+++ ItemSelectionItem.gd
@@ -15,11 +15,13 @@ func _ready() -> void:
 	$Price.text = "%d$" % cost
 
 func _on_TextureButton_pressed() -> void:
+	SoundEngine.play_sound("MenuButtonSound")
 	emit_signal("item_selected", item)
 
 
 func on_dropped(dropped_item) -> void:
 	if item == dropped_item:
+		SoundEngine.play_sound("Register")
 		get_tree().call_group("cost_counter", "add_cost", -cost)
 		cost += initial_cost
 		$Price.text = "%d$" % cost
